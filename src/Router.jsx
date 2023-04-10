0201f8d22aa72226afd5a6cff0f453b3b2a41ca0
ricardo-werner <wernerdive@gmail.com>
2023-04-10 02:32:13 -0300

    second commit

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,13 +1,3 @@
-/**
- * Cadastre as rotas da sua aplicação!
- * Lembrando: a estrutura do objeto que configura a rota é a seguinte:
- *
- * {
- *   path: "/caminho-da-rota",
- *   element: <ComponenteDaRota />,
- * }
- */
-
 import { createBrowserRouter } from "react-router-dom";
 import { QuemSou } from "./pages/QuemSou";
 import { Home } from "./pages/Home";
